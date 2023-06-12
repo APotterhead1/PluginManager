6cfeb9bcc2d89ae1475a4ffabcca3861e80291f6
APotterhead1 <craig.foulkrod@gmail.com>
2023-06-12 15:42:58 -0400

    Initial Commit

---
+++ src/main/java/me/apotterhead/pluginmanager/PluginManager.java
@@ -0,0 +1,17 @@
+package me.apotterhead.pluginmanager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PluginManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
