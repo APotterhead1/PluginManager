71db465129cb7f1e2e22316d7cb3210c68f9fa3c
APotterhead1 <craig.foulkrod@gmail.com>
2023-08-01 00:10:21 -0400

    implemented permission list subcommand
    Signed-off-by: APotterhead1 <craig.foulkrod@gmail.com>

--- src/main/java/me/apotterhead/pluginmanager/commands/PermissionCommand.java
+++ src/main/java/me/apotterhead/pluginmanager/commands/PermissionCommand.java
@@ -1,5 +1,5 @@
 // APotterhead
-// 22072023-25072023
+// 22072023-31072023
 
 package me.apotterhead.pluginmanager.commands;
 
@@ -16,6 +16,10 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import java.util.logging.Level;
 import net.kyori.adventure.text.TextComponent;
+import org.bukkit.permissions.Permission;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.event.ClickEvent;
+import java.util.Collections;
 
 public class PermissionCommand implements TabExecutor {
 
@@ -53,11 +57,11 @@ public class PermissionCommand implements TabExecutor {
                     return true;
                 }
 
-                String perm = args[1].replace( '.', ',' );
+                String permPath = args[1].replace( '.', ',' );
 
-                boolean originalValue = plugin.permissions.config.getBoolean( perm + ".negative" );
+                boolean originalValue = plugin.permissions.config.getBoolean( permPath + ".negative" );
 
-                plugin.permissions.config.set( perm + ".negative", Boolean.valueOf( args[2] ) );
+                plugin.permissions.config.set( permPath + ".negative", Boolean.valueOf( args[2] ) );
                 plugin.permissions.save();
 
                 Component reloadMessage = ReloadPermissions.reload( ReloadType.PERMISSION, args[1], plugin );
@@ -66,18 +70,48 @@ public class PermissionCommand implements TabExecutor {
                     sender.sendMessage( reloadMessage );
                     plugin.getLogger().log( Level.WARNING, ( (TextComponent) reloadMessage ).content() );
 
-                    plugin.permissions.config.set( perm + ".negative", originalValue );
+                    plugin.permissions.config.set( permPath + ".negative", originalValue );
                     plugin.permissions.save();
 
                     ReloadPermissions.reload( ReloadType.PERMISSION, args[1], plugin );
                     return true;
                 }
 
-                sender.sendMessage( Component.text( "Permission '" + args[1] + "' has been set to " + args[2] ).color( NamedTextColor.GREEN ) );
+                sender.sendMessage( Component.text( "Permission '" + args[1] + "' has been set to " + ( Boolean.parseBoolean( args[2] ) ? "negative" : "positive" ) ).color( NamedTextColor.GREEN ) );
                 return true;
             }
 
             sender.sendMessage( CommandErrorMessage.EXTRA_ARGUMENT.send( label, args, 3 ) );
+            return true;
+        }
+
+        if( args[0].equals( "list" ) ) {
+            if( !sender.hasPermission( "appm.commands.permission.list" ) ) {
+                sender.sendMessage( CommandErrorMessage.INCORRECT.send( label, args, 0 ) );
+                return true;
+            }
+
+            if( args.length == 1 ) {
+                Component message = Component.text( "" ).append( Component.text( "Permissions:" ).color( NamedTextColor.GOLD ) ).appendNewline();
+
+                List<Permission> permissions = new ArrayList<>( plugin.getServer().getPluginManager().getPermissions() );
+                List<String> perms = new ArrayList<>();
+                for( Permission perm : permissions ) perms.add( perm.getName() );
+                Collections.sort( perms );
+
+                if( sender.hasPermission( "appm.commands.permission.get.permission" ) ) {
+                    message = message.append( Component.text( "[" + perms.get( 0 ) + "]" ).decorate( TextDecoration.UNDERLINED ).clickEvent( ClickEvent.runCommand( "/permission get " + perms.get( 0 ) ) ) );
+                    for( int i = 1; i < perms.size(); i++ ) message = message.append( Component.text( ", " ) ).append( Component.text( "[" + perms.get( i ) + "]" ).decorate( TextDecoration.UNDERLINED ).clickEvent( ClickEvent.runCommand( "/permission get " + perms.get( i ) ) ) );
+                } else {
+                    message = message.append( Component.text( "[" + perms.get( 0 ) + "]" ) );
+                    for( int i = 1; i < perms.size(); i++ ) message = message.append( Component.text( ", [" + perms.get( 0 ) + "]" ) );
+                }
+
+                sender.sendMessage( message );
+                return true;
+            }
+
+            sender.sendMessage( CommandErrorMessage.EXTRA_ARGUMENT.send( label, args, 2 ) );
             return true;
         }
 
